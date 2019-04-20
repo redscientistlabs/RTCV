c80cb5733970c0269fdac98dce8928eef3190274
Dan B (Narry) <smellyfeetyouhave@gmail.com>
2019-04-19 22:58:05 -0400

    Remove tests from ComponentSubForm

--- Source/Frontend/UI/Modular/UI_ComponentFormSubForm.Designer.cs
+++ Source/Frontend/UI/Modular/UI_ComponentFormSubForm.Designer.cs
@@ -30,8 +30,6 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UI_ComponentFormSubForm));
             this.label1 = new System.Windows.Forms.Label();
-            this.button3 = new System.Windows.Forms.Button();
-            this.savestateList1 = new RTCV.UI.Components.Controls.SavestateList();
             this.SuspendLayout();
             // 
             // label1
@@ -45,32 +43,12 @@
             this.label1.TabIndex = 9;
             this.label1.Text = "Dummy Sub Form";
             // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(209, 285);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 13;
-            this.button3.Text = "Add Dummy";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.Button3_Click);
-            // 
-            // savestateList1
-            // 
-            this.savestateList1.DataSource = null;
-            this.savestateList1.Location = new System.Drawing.Point(59, 12);
-            this.savestateList1.Name = "savestateList1";
-            this.savestateList1.Size = new System.Drawing.Size(128, 296);
-            this.savestateList1.TabIndex = 10;
-            // 
             // UI_ComponentFormSubForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(96)))), ((int)(((byte)(96)))), ((int)(((byte)(96)))));
             this.ClientSize = new System.Drawing.Size(475, 320);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.savestateList1);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -86,7 +64,5 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private Components.Controls.SavestateList savestateList1;
-        private System.Windows.Forms.Button button3;
     }
 }
