5c0587b23ccde78971838283367c8d855276be61
NarryG <me@narry.land>
2019-04-23 22:54:46 -0400

    Fix MultiUpDown not firing updated events properly

--- Source/Frontend/UI/Components/Controls/MultiUpDown.cs
+++ Source/Frontend/UI/Components/Controls/MultiUpDown.cs
@@ -12,72 +12,87 @@ using RTCV.UI.Components.Controls;
 namespace RTCV.UI.Components.Controls
 {
 
-	public partial class MultiUpDown : SpecControl<decimal>
-	{
+    public partial class MultiUpDown : SpecControl<decimal>
+    {
 
-		[Description("Whether or not the NumericUpDown should use hex"), Category("Data")]
-		public bool Hexadecimal
-		{
-			get => updown.Hexadecimal;
-			set => updown.Hexadecimal = value;
-		}
-		[Description("The minimum value of the NumericUpDown"), Category("Data")]
-		public decimal Minimum
-		{
-			get => updown.Minimum;
-			set => updown.Minimum = value;
-		}
-		[Description("The maximum value of the NumericUpDown"), Category("Data")]
-		public decimal Maximum
-		{
-			get => updown.Maximum;
-			set => updown.Maximum = value;
-		}
+        [Description("Whether or not the NumericUpDown should use hex"), Category("Data")]
+        public bool Hexadecimal
+        {
+            get => updown.Hexadecimal;
+            set => updown.Hexadecimal = value;
+        }
 
-		public MultiUpDown()
-		{
-			InitializeComponent();
-			ForeColorChanged += (o, a) => updown.ForeColor = base.ForeColor.A == 255 ? base.ForeColor : Color.FromArgb(base.ForeColor.R, base.ForeColor.G, base.ForeColor.B);
+        [Description("The minimum value of the NumericUpDown"), Category("Data")]
+        public decimal Minimum
+        {
+            get => updown.Minimum;
+            set
+            {
+                //If the minimum is going to change the current value, we need to mark initialized as false at the end
+                bool reinit = value < updown.Value;
+                updown.Minimum = value;
+                initialized = reinit;
+            }
+        }
+
+        [Description("The maximum value of the NumericUpDown"), Category("Data")]
+        public decimal Maximum
+        {
+            get => updown.Maximum;
+            set
+            {
+
+                //If the minimum is going to change the current value, we need to mark initialized as false at the end
+                bool reinit = value > updown.Value;
+                updown.Maximum = value;
+                initialized = reinit;
+            }
+        }
+
+        public MultiUpDown()
+        {
+            InitializeComponent();
+            ForeColorChanged += (o, a) => updown.ForeColor = base.ForeColor.A == 255 ? base.ForeColor : Color.FromArgb(base.ForeColor.R, base.ForeColor.G, base.ForeColor.B);
             BackColorChanged += (o, a) => updown.BackColor = base.BackColor.A == 255 ? base.BackColor : Color.FromArgb(base.BackColor.R, base.BackColor.G, base.BackColor.B);
 
 
             updown.Tag = base.Tag;
             updown.ValueChanged += updown_ValueChanged;
-		}
+        }
 
         internal override void UpdateAllControls(decimal value, Control setter, bool ignore = false)
         {
-			GeneralUpdateFlag = true;
-			if (setter != this || ignore)
-			{
+            GeneralUpdateFlag = true;
+            if (setter != this || ignore)
+            {
                 updown.Value = value;
 
-				foreach (var slave in slaveComps)
-					slave.UpdateAllControls(value, this);
+                foreach (var slave in slaveComps)
+                    slave.UpdateAllControls(value, this);
 
-				if (_parent != null)
-					_parent.UpdateAllControls(value, setter);
+                if (_parent != null)
+                    _parent.UpdateAllControls(value, setter);
 
-			}
+            }
 
-			GeneralUpdateFlag = false;
+            GeneralUpdateFlag = false;
         }
 
         public void registerSlave(MultiUpDown comp)
-		{
-			comp.Minimum = this.Minimum;
-			comp.Maximum = this.Maximum;
-			base.registerSlave(comp);
-		}
+        {
+            comp.Minimum = this.Minimum;
+            comp.Maximum = this.Maximum;
+            base.registerSlave(comp);
+        }
 
-		private void updown_ValueChanged(object sender, EventArgs e)
-		{
-			if (GeneralUpdateFlag)
-				return;
+        private void updown_ValueChanged(object sender, EventArgs e)
+        {
+            if (GeneralUpdateFlag)
+                return;
 
-			PropagateValue(updown.Value, updown);
-		}
+            PropagateValue(updown.Value, updown);
+        }
 
 
-	}
+    }
 }
