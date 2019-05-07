88d5a83083c0a7171874a7b8e24673d6c274534a
Dan B (Narry) <smellyfeetyouhave@gmail.com>
2019-05-06 21:59:04 -0400

    Removed unused legacy code

--- Source/Libraries/CorruptCore/Generator/Iterator.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace RTCV.CorruptCore
-{
-    //Contains hooks to internal emulator timers or external timers for running auto-corrupt
-}
