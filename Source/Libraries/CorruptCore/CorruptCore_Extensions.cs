7c71a1430620b7da6e4989d24b3f18e6fdaec62d
Dan B <me@narry.land>
2019-05-12 10:33:17 -0400

    Remove unneeded targets

--- Source/Libraries/CorruptCore/CorruptCore_Extensions.cs
+++ Source/Libraries/CorruptCore/CorruptCore_Extensions.cs
@@ -877,87 +877,53 @@ namespace RTCV.CorruptCore
 		}
 	}
 
-	/// <summary>
-	/// Reference Article http://www.codeproject.com/KB/tips/SerializedObjectCloner.aspx
-	/// Provides a method for performing a deep copy of an object.
-	/// Binary Serialization is used to perform the copy.
-	/// </summary>
-	public static class ObjectCopier
-	{
-		/// <summary>
-		/// Perform a deep Copy of the object.
-		/// </summary>
-		/// <typeparam name="T">The type of object being copied.</typeparam>
-		/// <param name="source">The object instance to copy.</param>
-		/// <returns>The copied object.</returns>
-		public static T Clone<T>(T source)
-		{
-			if (!typeof(T).IsSerializable)
-			{
-				throw new ArgumentException("The type must be serializable.", "source");
-			}
 
-			// Don't serialize a null object, simply return the default for that object
-			if (Object.ReferenceEquals(source, null))
-			{
-				return default(T);
-			}
-
-			IFormatter formatter = new BinaryFormatter();
-			Stream stream = new MemoryStream();
-			using (stream)
-			{
-				formatter.Serialize(stream, source);
-				stream.Seek(0, SeekOrigin.Begin);
-				return (T)formatter.Deserialize(stream);
-			}
-		}
-	}
-	/// <summary>
-	/// Reference Article http://www.codeproject.com/KB/tips/SerializedObjectCloner.aspx
-	/// Provides a method for performing a deep copy of an object.
-	/// Binary Serialization is used to perform the copy.
-	/// </summary>
-	public static class ObjectCopierCeras
-	{
-		/// <summary>
-		/// Perform a deep Copy of the object.
-		/// </summary>
-		/// <typeparam name="T">The type of object being copied.</typeparam>
-		/// <param name="source">The object instance to copy.</param>
-		/// <returns>The copied object.</returns>
-		public static T Clone<T>(T source)
-		{
-			if (!typeof(T).IsSerializable)
-			{
-				throw new ArgumentException("The type must be serializable.", "source");
-			}
-
-			// Don't serialize a null object, simply return the default for that object
-			if (Object.ReferenceEquals(source, null))
-			{
-				return default(T);
-			}
-
-
-			var config = new SerializerConfig();
-			config.DefaultTargets = TargetMember.All;
-			//config.Advanced.ReadonlyFieldHandling = ReadonlyFieldHandling.ForcedOverwrite;
-			/*config.OnConfigNewType = t =>
+    public static class ObjectCopier
+    {
+        public static T Clone<T>(T source)
+        {
+            if (!typeof(T).IsSerializable)
             {
-                foreach(var m in t.Members)
-				{
-					m.SerializationOverride = SerializationOverride.ForceInclude;
-				}
-            };*/
-			//config.Advanced. = m => SerializationOverride.ForceInclude;
-			var s = new CerasSerializer(config);
+                throw new ArgumentException("The type must be serializable.", "source");
+            }
 
-			return s.Deserialize<T>(s.Serialize(source));
-		}
-	}
+            //Return default of a null object
+            if (Object.ReferenceEquals(source, null))
+            {
+                return default(T);
+            }
 
+            using (MemoryStream stream = new MemoryStream())
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                formatter.Serialize(stream, source);
+                stream.Seek(0, SeekOrigin.Begin);
+                return (T)formatter.Deserialize(stream);
+            }
+        }
+    }
 
+    public static class ObjectCopierCeras
+    {
+        public static T Clone<T>(T source)
+        {
+            if (!typeof(T).IsSerializable)
+            {
+                throw new ArgumentException("The type must be serializable.", "source");
+            }
+
+            //Return default of a null object
+            if (Object.ReferenceEquals(source, null))
+            {
+                return default(T);
+            }
+
+            var config = new SerializerConfig();
+            config.DefaultTargets = TargetMember.All;
+            var s = new CerasSerializer(config);
+            return s.Deserialize<T>(s.Serialize(source));
+        }
+    }
 	//Export dgv to csv
 	public class CSVGenerator
 	{
--- Source/Libraries/NetCore/Extensions.cs
+++ Source/Libraries/NetCore/Extensions.cs
@@ -15,37 +15,26 @@ using Newtonsoft.Json.Serialization;
 namespace RTCV.NetCore
 {
 	public class Extensions
-	{   /// <summary>
-		/// Reference Article http://www.codeproject.com/KB/tips/SerializedObjectCloner.aspx
-		/// Provides a method for performing a deep copy of an object.
-		/// Binary Serialization is used to perform the copy.
-		/// </summary>
+	{  
 		public static class ObjectCopier
 		{
-			/// <summary>
-			/// Perform a deep Copy of the object.
-			/// </summary>
-			/// <typeparam name="T">The type of object being copied.</typeparam>
-			/// <param name="source">The object instance to copy.</param>
-			/// <returns>The copied object.</returns>
 			public static T Clone<T>(T source)
-			{
-				if (!typeof(T).IsSerializable)
-				{
-					throw new ArgumentException("The type must be serializable.", "source");
-				}
+            {
+                if (!typeof(T).IsSerializable)
+                {
+                    throw new ArgumentException("The type must be serializable.", "source");
+                }
 
-				// Don't serialize a null object, simply return the default for that object
-				if (Object.ReferenceEquals(source, null))
+                //Return default of a null object
+                if (Object.ReferenceEquals(source, null))
 				{
 					return default(T);
 				}
 
-				IFormatter formatter = new BinaryFormatter();
-				Stream stream = new MemoryStream();
-				using (stream)
-				{
-					formatter.Serialize(stream, source);
+				using (MemoryStream stream = new MemoryStream())
+                {
+                    BinaryFormatter formatter = new BinaryFormatter();
+                    formatter.Serialize(stream, source);
 					stream.Seek(0, SeekOrigin.Begin);
 					return (T)formatter.Deserialize(stream);
 				}
