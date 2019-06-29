6d8cc496df975fb23569ffba4fab7146f19e3147
Dan B <me@narry.land>
2019-06-28 21:56:58 -0400

    0.2.0

--- Source/Libraries/CorruptCore/CorruptCore.cs
+++ Source/Libraries/CorruptCore/CorruptCore.cs
@@ -19,7 +19,7 @@ namespace RTCV.CorruptCore
 	public static class RtcCore
 	{
 		//General RTC Values
-		public static string RtcVersion = "0.1.9";
+		public static string RtcVersion = "0.2.0";
 
         private static volatile int seed = DateTime.Now.Millisecond;
         public static int Seed => ++seed;
