3d30f9abd9c8b3d5ff79fd668c08b98f533de145
Dan B <me@narry.land>
2019-12-29 03:53:30 -0500

    504-b4

--- Source/Libraries/CorruptCore/CorruptCore.cs
+++ Source/Libraries/CorruptCore/CorruptCore.cs
@@ -22,7 +22,7 @@ namespace RTCV.CorruptCore
 	public static class RtcCore
 	{
         //General RTC Values
-        public const string RtcVersion = "5.0.4-b3";
+        public const string RtcVersion = "5.0.4-b4";
 		private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
 
         private static volatile int seed = DateTime.Now.Millisecond;
