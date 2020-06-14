590ce4071e6d365908678f651664c427fdb33072
Scott Walters <me@scowalt.com>
2020-06-14 16:17:22 -0700

    Fixed SA1120

--- Source/Frontend/UI/Input/Input.cs
+++ Source/Frontend/UI/Input/Input.cs
@@ -36,19 +36,19 @@
             // Summary:
             //     The bitmask to extract modifiers from a key value.
             Modifiers = -65536,
-            //
+
             // Summary:
             //     No key pressed.
             None = 0,
-            //
+
             // Summary:
             //     The SHIFT modifier key.
             Shift = 65536,
-            //
+
             // Summary:
             //     The CTRL modifier key.
             Control = 131072,
-            //
+
             // Summary:
             //     The ALT modifier key.
             Alt = 262144,
