17035ec89ca3b6722a4382bf94c393cffd024a50
NarryG <me@narry.land>
2020-06-14 22:50:48 -0400

    505-b8

--- Source/Libraries/CorruptCore/CorruptCore.cs
+++ Source/Libraries/CorruptCore/CorruptCore.cs
@@ -20,7 +20,7 @@ namespace RTCV.CorruptCore
     public static class RtcCore
     {
         //General RTC Values
-        public const string RtcVersion = "5.0.5-b7";
+        public const string RtcVersion = "5.0.5-b8";
         private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
 
         private static volatile int seed = DateTime.Now.Millisecond;
