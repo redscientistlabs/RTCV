eede306766e7dbb28ed7d953d1aca5164f8c9074
Scott Walters <me@scowalt.com>
2020-07-24 08:21:43 -0700

    Last classes remove, file deleted

--- Source/Libraries/CorruptCore/ProgressBar.cs
+++ Source/Libraries/CorruptCore/ProgressBar.cs
@@ -2,32 +2,6 @@ namespace RTCV.CorruptCore
 {
     using System;
 
-    [Serializable]
-    public class ActiveTableObject
-    {
-        public long[] Data { get; set; }
-
-        public ActiveTableObject()
-        {
-        }
-
-        public ActiveTableObject(long[] data)
-        {
-            Data = data;
-        }
-    }
-
-    public class ProblematicProcess
-    {
-        public string Name { get; set; }
-        public string Message { get; set; }
-    }
-
-    public interface INote
-    {
-        string Note { get; set; }
-    }
-
     public delegate void ProgressBarEventHandler(object source, ProgressBarEventArgs e);
 
     public class ProgressBarEventArgs : EventArgs
