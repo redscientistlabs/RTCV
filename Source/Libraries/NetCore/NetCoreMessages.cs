5308f2b35edd667ef414bdae17c67f7939a67329
Scott Walters <me@scowalt.com>
2020-08-29 05:37:06 -0700

    [Refactor] Move NetCoreMessage classes to their own file

---
+++ Source/Libraries/NetCore/NetCoreMessages.cs
@@ -0,0 +1,49 @@
+namespace RTCV.NetCore
+{
+    using System;
+    using Ceras;
+
+    [Serializable()]
+    [Ceras.MemberConfig(TargetMember.All)]
+    public abstract class NetCoreMessage
+    {
+        public string Type;
+    }
+
+    [Serializable()]
+    [Ceras.MemberConfig(TargetMember.All)]
+    public class NetCoreSimpleMessage : NetCoreMessage
+    {
+        public NetCoreSimpleMessage()
+        {
+        }
+        public NetCoreSimpleMessage(string _Type)
+        {
+            Type = _Type.Trim().ToUpper();
+        }
+    }
+
+    [Serializable()]
+    [Ceras.MemberConfig(TargetMember.All)]
+    public class NetCoreAdvancedMessage : NetCoreMessage
+    {
+        public string ReturnedFrom;
+        public bool Priority = false;
+        public Guid? requestGuid = null;
+        public object objectValue = null;
+
+        public NetCoreAdvancedMessage()
+        {
+        }
+        public NetCoreAdvancedMessage(string _Type)
+        {
+            Type = _Type.Trim().ToUpper();
+        }
+
+        public NetCoreAdvancedMessage(string _Type, object _Obj)
+        {
+            Type = _Type.Trim().ToUpper();
+            objectValue = _Obj;
+        }
+    }
+}
