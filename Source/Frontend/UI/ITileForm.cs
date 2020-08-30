1ca465c569732723e3a407ee10030b705c57ec38
Scott Walters <me@scowalt.com>
2020-08-30 15:25:11 -0700

    Another formatting fix

--- Source/Frontend/UI/ITileForm.cs
+++ Source/Frontend/UI/ITileForm.cs
@@ -3,7 +3,6 @@
     public interface ITileForm
     {
         //Interface used for added contrals in TileForms
-
         int TilesX { get; set; }
         int TilesY { get; set; }
         bool CanPopout { get; set; }
