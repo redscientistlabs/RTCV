bcb10797eb5ec628d73bffab4e318175da998b85
Scott Walters <me@scowalt.com>
2020-08-30 15:19:35 -0700

    UIObjects

--- Source/Frontend/UI/IBlockable.cs
+++ Source/Frontend/UI/IBlockable.cs
@@ -1,15 +1,6 @@
-﻿namespace RTCV.UI
+namespace RTCV.UI
 {
     using System.Windows.Forms;
-
-    public interface ITileForm
-    {
-        //Interface used for added contrals in TileForms
-
-        int TilesX { get; set; }
-        int TilesY { get; set; }
-        bool CanPopout { get; set; }
-    }
 
     public interface IBlockable
     {
