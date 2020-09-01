11cb51a0e9f692dab01dbc116e4d98ce6d57b2d8
Scott Walters <me@scowalt.com>
2020-09-01 16:32:21 -0700

    BlastTarget revert

--- Source/Libraries/CorruptCore/BlastTarget.cs
+++ Source/Libraries/CorruptCore/BlastTarget.cs
@@ -1,14 +1,18 @@
 namespace RTCV.CorruptCore
 {
     using System;
+    using System.Diagnostics.CodeAnalysis;
     using Ceras;
 
     [Serializable]
     [MemberConfig(TargetMember.All)]
     public class BlastTarget
     {
-        public string Domain { get; private set; } = null;
-        public long Address { get; private set; } = 0;
+        [SuppressMessage("Microsoft.Design", "CA1051", Justification = "Unknown serialization impact of making this property instead of a field")]
+        public string Domain = null;
+
+        [SuppressMessage("Microsoft.Design", "CA1051", Justification = "Unknown serialization impact of making this property instead of a field")]
+        public long Address = 0;
 
         public BlastTarget(string _domain, long _address)
         {
