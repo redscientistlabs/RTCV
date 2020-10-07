694f5bda78182b53342cf55332bcd133ac67dc37
Scott Walters <me@scowalt.com>
2020-10-07 08:34:34 -0700

    Using FluentAssertions instead of modifying object

--- Source/Libraries/CorruptCore/ActivationTableObject.cs
+++ Source/Libraries/CorruptCore/ActivationTableObject.cs
@@ -1,13 +1,9 @@
 namespace RTCV.CorruptCore
 {
     using System;
-    using System.Diagnostics.CodeAnalysis;
-    using System.Linq;
-    using System.Runtime.CompilerServices;
-    using Newtonsoft.Json;
 
     [Serializable]
-    public class ActiveTableObject : IEquatable<ActiveTableObject>
+    public class ActiveTableObject
     {
         public long[] Data { get; set; }
 
@@ -18,26 +14,6 @@ namespace RTCV.CorruptCore
         public ActiveTableObject(long[] data)
         {
             Data = data;
-        }
-
-        public bool Equals(ActiveTableObject other)
-        {
-            if (other == null)
-            {
-                return this == null;
-            }
-
-            return Enumerable.SequenceEqual(Data, other.Data);
-        }
-
-        public override bool Equals(object obj)
-        {
-            return Equals(obj as ActiveTableObject);
-        }
-
-        public override int GetHashCode()
-        {
-            return Data.GetHashCode();
         }
     }
 }
--- Tests/SerializationTests/CorruptCoreSerializationTest.cs
+++ Tests/SerializationTests/CorruptCoreSerializationTest.cs
@@ -3,6 +3,7 @@ namespace SerializationTests
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using Newtonsoft.Json;
     using RTCV.CorruptCore;
+    using FluentAssertions;
 
     [TestClass]
     public class CorruptCoreSerializationTest
@@ -15,7 +16,7 @@ namespace SerializationTests
             var serialized = JsonConvert.SerializeObject(activeTableObject);
             var deserializedActiveTableObject = JsonConvert.DeserializeObject<ActiveTableObject>(serialized);
 
-            Assert.IsTrue(condition: activeTableObject.Equals(deserializedActiveTableObject));
+            deserializedActiveTableObject.Should().BeEquivalentTo(activeTableObject);
         }
     }
 }
