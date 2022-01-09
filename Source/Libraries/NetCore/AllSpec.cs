c2513fdb2a9ad74c5911000008190e1e1cd9fb60
removedporn <86824510+removedporn@users.noreply.github.com>
2022-01-09 19:44:17 +0800

    Delete Source directory

--- Source/Libraries/NetCore/AllSpec.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RTCV.NetCore
-{
-    public static class AllSpec
-    {
-        public static volatile FullSpec CorruptCoreSpec;
-        public static volatile FullSpec VanguardSpec;
-        public static volatile FullSpec UISpec;
-    }
-}
